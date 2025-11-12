16b0911e11a60e5fb8c4b9d64fd61d63426bfa59
Jormaedes Luís <jormahed@gmail.com>
2025-11-12 17:57:50 +0100

    criei a funcao que atualiza os pontos na pagina

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const btns = document.querySelectorAll('button');
+const ppoint = document.querySelector('#ppoint');
+const cpoint = document.querySelector('#cpoint');
+const divStatus = document.querySelector('#status');
+let playerPoint = 0;
+let computerPoint = 0;
 
 function getComputerChoice() {
 	const	options = ['rock', 'paper', 'scissors'];
 	return options[(Math.floor(Math.random() * 3))];
+}
+
+function getWinner(humanChoice, computerChoice){
+	humanChoice = humanChoice.toLocaleLowerCase();
+	computerChoice = computerChoice.toLocaleLowerCase();
+	console.log(humanChoice, computerChoice);
+	if (humanChoice == computerChoice){
+		return (0);
+	}
+	if ((humanChoice == 'rock' && computerChoice == 'scissors')
+		|| (humanChoice == 'paper' && computerChoice == 'rock')
+		|| (humanChoice == 'scissors' && computerChoice == 'paper')) {
+			return (1);
+	}else {
+		return (2);
+	}
+}
+
+function updatePoints(){
+	ppoint.textContent = playerPoint.toString();
+	cpoint.textContent = computerPoint.toString();
 }
 
 function playRound(target) {
@@ -15,7 +41,16 @@ function playRound(target) {
 	else
 		choicePlayer = 'scissors';
 	const choiceComputer = getComputerChoice();
-
+	const winner = getWinner(choicePlayer, choiceComputer);
+	if (winner == 1)
+		playerPoint++;
+	else if (winner == 2)
+		computerPoint++;
+	updatePoints();
+	if (playerPoint == 5)
+		divStatus.innerHTML = "<span class='green'>You win</span>";
+	else if (computerPoint == 5)
+		divStatus.innerHTML = "<span class='red'>You lose</span>";
 }
 
 for(const b of btns) {
