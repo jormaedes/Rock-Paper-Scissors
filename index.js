62861c53b5f238a91a85124940e2a7524a1c88b9
Jormaedes Luís <jormahed@gmail.com>
2025-11-12 14:45:28 +0100

    Comecei com a logica do jogo

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
+const btns = document.querySelectorAll('button');
+
 function getComputerChoice() {
-	let choice = Math.floor(Math.random() * 3);
-	if (choice == 0) return ("Rock");
-	else if (choice == 1) return ("Paper");
-	return ("Scissors");
+	const	options = ['rock', 'paper', 'scissors'];
+	return options[(Math.floor(Math.random() * 3))];
 }
 
-function playRound(humanChoice, computerChoice){
-	humanChoice = humanChoice.toLocaleLowerCase();
-	computerChoice = computerChoice.toLocaleLowerCase();
-	if (humanChoice == computerChoice){
-		console.log("Draw");
-		return (0);
-	}
-	if ((humanChoice == 'rock' && computerChoice == 'scissors')
-		|| (humanChoice == 'paper' && computerChoice == 'rock')
-		|| (humanChoice == 'scissors' && computerChoice == 'paper')) {
-			console.log("You won! " + humanChoice + " beats " + computerChoice);
-			return (1);
-	}else {
-		console.log("You lose! " + computerChoice + " beats " + humanChoice);
-		return (2);
-	}
-}
+function playRound(target) {
+	let	choicePlayer;
 
-function getHumanChoice(){
-	let choice = prompt("Choice: Rock, Paper or Scissors")
-	return (choice);
-}
-
-
-function playGame(){
-	let humanScore = 0;
-	let computerScore = 0;
-
-	for(let i = 0; i < 5; i++){
-		const humanSelection = getHumanChoice();
-		const computerSelection = getComputerChoice();
-		let winner = playRound(humanSelection, computerSelection);
-		if (winner == 1)
-			humanScore++;
-		else if (winner == 2)
-			computerScore++;
-	}
-	if (humanScore == computerScore)
-		console.log(`Draw! Final score - Player: ${humanScore} | Computer: ${computerScore}`);
-	else if (humanScore > computerScore)
-		console.log(`You won! Final score - Player: ${humanScore} | Computer: ${computerScore}`);
+	if (target.getAttribute('id') == 'btn-rock')
+		choicePlayer = 'rock';
+	else if (target.getAttribute('id') == 'btn-paper')
+		choicePlayer = 'paper';
 	else
-		console.log(`You lose! Final score - Player: ${humanScore} | Computer: ${computerScore}`);
+		choicePlayer = 'scissors';
+	const choiceComputer = getComputerChoice();
+
 }
 
-playGame();
+for(const b of btns) {
+	b.addEventListener('click', (e)=>{
+		playRound(e.target);
+	});
+}
