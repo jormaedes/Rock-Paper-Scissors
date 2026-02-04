100bdb8c25d5df42f49c1588da986ec89453d6b6
Jormaedes Luís <jormahed@gmail.com>
2026-02-04 17:47:51 +0100

    primeira parte da logica ja foi feita

--- index.js
+++ index.js
@@ -1,71 +1,28 @@
-const btns = document.querySelectorAll('button');
-const ppoint = document.querySelector('#ppoint');
-const cpoint = document.querySelector('#cpoint');
-const divStatus = document.querySelector('#status');
-let playerPoint = 0;
-let computerPoint = 0;
+const btnPlay = document.getElementById('play-button');
+const btnReset = document.getElementById('reset-button');
+const playerPoints = document.getElementById('player-score');
+const computerPoints = document.getElementById('computer-score');
+const choicesButtons =[...document.querySelectorAll('.choices-container button')];
+const dialog = document.querySelector('dialog');
 
 function getComputerChoice() {
-	const	options = ['rock', 'paper', 'scissors'];
-	return options[(Math.floor(Math.random() * 3))];
+	const choices = ['rock', 'paper', 'scissors'];
+	const randomIndex = Math.floor(Math.random() * choices.length);
+	return choices[randomIndex];
 }
 
-function getWinner(humanChoice, computerChoice){
-	humanChoice = humanChoice.toLocaleLowerCase();
-	computerChoice = computerChoice.toLocaleLowerCase();
-	console.log(humanChoice, computerChoice);
-	if (humanChoice == computerChoice){
-		divStatus.textContent = "Draw";
-		return (0);
-	}
-	if ((humanChoice == 'rock' && computerChoice == 'scissors')
-		|| (humanChoice == 'paper' && computerChoice == 'rock')
-		|| (humanChoice == 'scissors' && computerChoice == 'paper')) {
-			divStatus.textContent = "You win this round! " + humanChoice + " beats " + computerChoice;
-			return (1);
-	}else {
-		divStatus.textContent = "You lose this round! " + computerChoice + " beats " + humanChoice;
-		return (2);
-	}
-}
-
-function updatePoints(){
-	ppoint.textContent = playerPoint.toString();
-	cpoint.textContent = computerPoint.toString();
-}
-
-function playRound(target) {
-	let	choicePlayer;
-
-	if (target.getAttribute('id') == 'btn-rock')
-		choicePlayer = 'rock';
-	else if (target.getAttribute('id') == 'btn-paper')
-		choicePlayer = 'paper';
-	else
-		choicePlayer = 'scissors';
-	const choiceComputer = getComputerChoice();
-	const winner = getWinner(choicePlayer, choiceComputer);
-	if (winner == 1)
-		playerPoint++;
-	else if (winner == 2)
-		computerPoint++;
-	updatePoints();
-	if (playerPoint == 5){
-		divStatus.innerHTML = "<span class='green'>You win</span>";
-		alert('You win');
-	}
-	else if (computerPoint == 5){
-		divStatus.innerHTML = "<span class='red'>You lose</span>";
-		alert('You lose');
-	}
-	if (playerPoint == 5 || computerPoint == 5) {
-		playerPoint = 0;
-		computerPoint = 0;
-	}
-}
-
-for(const b of btns) {
-	b.addEventListener('click', (e)=>{
-		playRound(e.target);
+choicesButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		choicesButtons.forEach(btn => btn.firstElementChild.classList.remove('selected'));
+		button.firstElementChild.classList.add('selected');
 	});
-}
+});
+
+btnPlay.addEventListener('click', () => {
+	const playerChoice = document.querySelector('.choices-container button img.selected');
+	if (!playerChoice) {
+		return;
+	}
+	dialog.showModal();
+	// Game logic here
+});
